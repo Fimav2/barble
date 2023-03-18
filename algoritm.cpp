45e594f12a6b20c584b8037695af573d438f4762
Я сын шлюхи <113976105+Fimav2@users.noreply.github.com>
2023-03-18 21:13:53 +0300

    Update algoritm.cpp

--- algoritm.cpp
+++ algoritm.cpp
@@ -19,3 +19,22 @@ vector <int> bubble_sort(vector <int> mass) {
 
     return mass;
 }
+vector <int> shaker_sort(vector <int> mass){
+    int f;
+    for(int i = 0;i < mass.size();i++)
+        for(int j = 0;j < mass.size()-1;j++){
+                if(mass[j] > mass[j+1]){
+                    int c = mass[j];
+                    mass[j] = mass[j+1];
+                    mass[j+1] = c;
+}}
+            for(int g = 0; g < mass.size()-1;g++){
+                    if(mass[g] > mass[g+1]){
+                    f = mass[g];
+                    mass[g] = mass[g+1];
+                    mass[g+1] = f;
+                    }
+            }
+            return mass;
+        }
+
